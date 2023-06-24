377ecb48099c395085b96e85e795c11790e7a226
Gautam Magar <puskarmagar687@gmail.com>
2023-06-25 02:35:10 +0545

    feat:creating responsive design some bugs fix creating footer create small device nav bar

---
+++ script.js
@@ -0,0 +1,19 @@
+function hide(){
+    // console.log('jbkbk');
+    document.getElementById('sm-nav').style.display = "none";
+    
+}
+
+function display(){
+    // console.log('hbj')
+    document.getElementById('sm-nav').style.display ="block";
+    document.getElementById('sm-nav').style.top = "0%";
+    // document.getElementById('sm-nav').style.right = "50%";
+    // document.getElementById('sm-nav').style.transition = "20s";
+
+}
+function none(){
+    document.getElementById('sm-nav').style.display = "none";
+
+
+}
